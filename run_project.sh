8dfc69e57359ed86b27e785708bed1986db7878b
shirelepfl <shirel.gelfged@epfl.ch>
2025-12-10 15:34:15 +0100

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Compile the C code
+gcc -O2 -o tumor_simulation C_code.c -lm
+
+# Run the C simulation
+./tumor_simulation
+
+# Run the MATLAB script (without opening GUI)
+matlab -nodisplay -nosplash -r "run('Matlab_code.m'); exit;"
