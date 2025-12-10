a47efee1bbdf1247f6159137ee13cd9eea63d0b2
shirelepfl <shirel.gelfged@epfl.ch>
2025-12-10 15:39:12 +0100

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Supprimer d'anciens fichiers pour repartir propre
+rm -f healthy_results.csv smoker_results.csv tumor_evolution.gif
+rm -rf frames_lungs
+
 # Compile the C code
 gcc -O2 -o tumor_simulation C_code.c -lm
 
