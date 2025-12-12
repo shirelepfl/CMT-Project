9e19188db800378cf260527da1ca33ee139dca16
rebi-lv <167556779+rebi-lv@users.noreply.github.com>
2025-12-12 11:26:14 +0100

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile the C program located in src/
-gcc -O2 -o src/output src/C_code.c -lm
+gcc -O2 -o results/output src/C_code.c -lm
 
 # Run the C simulation (output will appear in the project folder)
-./src/output
+./results/output
 
 # Run the MATLAB script located in src/
 /usr/local/bin/matlab-2021b -batch "run('src/Matlab_code.m')"
