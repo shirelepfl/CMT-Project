e669ffd3c49d9636b31430cafc5639fb19c22d6e
rebi-lv <167556779+rebi-lv@users.noreply.github.com>
2025-12-15 15:32:58 +0100

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install essential development libraries (includes stdio, stdlib, math, string)
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+
 # Compile the C program located in src/
 gcc -O2 -o results/output src/C_code.c -lm
 
